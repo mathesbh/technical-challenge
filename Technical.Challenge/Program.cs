2d0a5db00d08a0bb65a1a92cd24fc05bc797d753
Matheus Salomão <mathesbh@gmail.com>
2022-04-15 11:36:31 -0300

    adiciona números divisores no console

--- Technical.Challenge/Program.cs
+++ Technical.Challenge/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Technical.Challenge.Techical.Challenge.Models;
 
 namespace Technical.Challenge
 {
@@ -6,7 +7,18 @@ namespace Technical.Challenge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Digite um número: ");
+
+            var divisors = new Divisors();
+
+            var number = Convert.ToInt32(Console.ReadLine());
+
+            var numbers = divisors.NumberDivisors(number);
+
+            Console.WriteLine("Número digitado: {0}", number);
+
+            Console.WriteLine("Números divisores: {0}", string.Join(", ", numbers));
+
         }
     }
 }
