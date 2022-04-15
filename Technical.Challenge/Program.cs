b79ca0ef14017dd9673338842d5ad6cc7d1caa75
Matheus Salomão <mathesbh@gmail.com>
2022-04-15 10:41:06 -0300

    Adicionar arquivos de projeto.

---
+++ Technical.Challenge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Technical.Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
