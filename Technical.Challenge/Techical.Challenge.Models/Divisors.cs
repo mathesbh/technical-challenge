36c872b862df0f0d37e8f6ff5a1eb84158281973
Matheus Salomão <mathesbh@gmail.com>
2022-04-15 11:19:57 -0300

    adiciona class divisor

---
+++ Technical.Challenge/Techical.Challenge.Models/Divisors.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Technical.Challenge.Techical.Challenge.Models
+{
+    public class Divisors
+    {
+        public List<int> NumberDivisors(int number)
+        {
+            List<int> divisors = new List<int>();
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0) divisors.Add(i);
+            }
+
+            return divisors;
+        }
+    }
+}
