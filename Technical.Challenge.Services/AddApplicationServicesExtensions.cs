4ce3e9b717671972797e9e6876db97c548e57e39
Matheus Salomão <mathesbh@gmail.com>
2022-04-16 10:36:47 -0300

    adiciona classe DI

---
+++ Technical.Challenge.Services/AddApplicationServicesExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Technical.Challenge.Domain.Interfaces;
+
+namespace Technical.Challenge.Services
+{
+    public static class AddApplicationServicesExtensions
+    {
+        public static IServiceCollection AddApplicatonServices(this IServiceCollection services)
+        {
+            services.AddTransient<IDivisorsServices, DivisorsService>();
+
+            return services;
+        }
+    }
+}
