feaf13cd4549c36166181d47a1f5d1d3ed870e28
Matheus Salomão <mathesbh@gmail.com>
2022-04-16 12:23:04 -0300

    resolve dependencias e DI

--- Technical.Challenge.Api/Startup.cs
+++ Technical.Challenge.Api/Startup.cs
@@ -1,16 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Technical.Challenge.Services;
 
 namespace Technical.Challenge.Api
 {
@@ -26,7 +20,7 @@ namespace Technical.Challenge.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            AddApplicationServicesExtensions.AddApplicatonServices(services);
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
